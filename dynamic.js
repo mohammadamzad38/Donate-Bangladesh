130a2bbb9a642bfe9c16a9e2e05241948b16d766
mohammadamzad38 <hossainamzad38@gmail.com>
2024-10-17 15:03:20 +0600

    something

--- dynamic.js
+++ dynamic.js
@@ -1,10 +1,17 @@
-function preventbuttonloader (id){
-    document.getElementById()
-    .addEventListener('click', function(event){
-        event.preventDefault(id);
-    })
-}
+// function preventbuttonloader (id){
+//     document.getElementById()
+//     .addEventListener('click', function(event){
+//         event.preventDefault(id);
+//     })
+// }
 
+document.getElementById('donation-button').addEventListener('click', function(event){
+    event.preventDefault();
+})
+
+document.getElementById('history-button').addEventListener('click', function(event){
+    event.preventDefault();
+})
 // button switching
 
 document.getElementById('donation-button')
@@ -23,8 +30,21 @@ function showsection (id){
     document.getElementById('history-section').classList.add('hidden');
 
     document.getElementById(id).classList.remove('hidden');
+
+
+
+    document.getElementById('donation-button').classList.add('btnclr')
+    document.getElementById('history-button').classList.add('btnclr')
+
+    document.getElementById(id).classList.remove('btnclr');
 }
 
+// button color changing
+
+// function changeColor (event){
+//     var color = 'yellow'
+//     event.style.backgroundColor = color;
+// }
 
 
 
